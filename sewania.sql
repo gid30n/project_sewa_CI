03137aac98ff3c658335e6dfb147b27392bb35d3
suryadana <suryadana80@gmail.com>
2016-10-01 17:50:01 +0000

    Add table kategori

--- sewania.sql
+++ sewania.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 01 Okt 2016 pada 11.50
+-- Generation Time: 01 Okt 2016 pada 17.49
 -- Versi Server: 10.1.17-MariaDB
 -- PHP Version: 5.6.26
 
@@ -72,7 +72,7 @@ CREATE TABLE `tb_business_profile` (
 --
 
 INSERT INTO `tb_business_profile` (`id_business_profile`, `brand_name`, `type_service`, `address`, `id_province`, `id_region`, `description`, `id_user`) VALUES
-(1, 'uik dev', 'tes', 'jl anu', 1, 6, 'tes', 2);
+(1, 'uik dev', 'tes', 'jl anu', 1, 6, 'tes', 15);
 
 -- --------------------------------------------------------
 
@@ -82,8 +82,16 @@ INSERT INTO `tb_business_profile` (`id_business_profile`, `brand_name`, `type_se
 
 CREATE TABLE `tb_kategori` (
   `id_kategori` int(11) NOT NULL,
-  `title` int(11) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `tb_kategori`
+--
+
+INSERT INTO `tb_kategori` (`id_kategori`, `name`) VALUES
+(1, 'Peralatan Pesta'),
+(2, 'Paket Pesta');
 
 -- --------------------------------------------------------
 
@@ -138,6 +146,49 @@ INSERT INTO `tb_region` (`id_region`, `name`, `id_province`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `tb_sub_kategori`
+--
+
+CREATE TABLE `tb_sub_kategori` (
+  `id_sub_kategori` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `id_kategori` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `tb_sub_kategori`
+--
+
+INSERT INTO `tb_sub_kategori` (`id_sub_kategori`, `name`, `id_kategori`) VALUES
+(1, 'Tenda', 1),
+(2, 'Kursi', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_super_sub_kategori`
+--
+
+CREATE TABLE `tb_super_sub_kategori` (
+  `id_super_sub_kategori` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `id_sub_kategori` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `tb_super_sub_kategori`
+--
+
+INSERT INTO `tb_super_sub_kategori` (`id_super_sub_kategori`, `name`, `id_sub_kategori`, `id_kategori`) VALUES
+(1, 'Tenda Krucut', 1, 1),
+(2, 'Tenda Dekorasi', 1, 1),
+(3, 'Kursi Spon', 2, 1),
+(4, 'Kursi Plastik', 2, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tb_users`
 --
 
@@ -158,7 +209,8 @@ CREATE TABLE `tb_users` (
 --
 
 INSERT INTO `tb_users` (`id_user`, `first_name`, `last_name`, `email`, `password`, `joined`, `last_login`, `ip_last`, `admin`) VALUES
-(4, 'Komang', 'Suryadana', 'suryadana80@gmail.com', '8821716db3b4a9e6708a792da936df9357bb9b455e1600354c54d2009e6deb4275fc1224d33989452af7214bfbccde878174fc56d562ecfcc6f68c08a4fbef4e8pzcODt1xyBIbsuUc9phVgFc4C5RaUTMD1SXm+EdLhA=', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', 1);
+(2, 'Komang', 'Suryadana', 'suryadana80@gmail.com', '8821716db3b4a9e6708a792da936df9357bb9b455e1600354c54d2009e6deb4275fc1224d33989452af7214bfbccde878174fc56d562ecfcc6f68c08a4fbef4e8pzcODt1xyBIbsuUc9phVgFc4C5RaUTMD1SXm+EdLhA=', '0000-00-00 00:00:00', '2016-10-01 22:48:37', '127.0.0.1', 1),
+(15, 'Mikel', 'Eric', 'pendekar_langit@protonmail.com', 'dd314dadda935008202a1d1028e923bc0d0bbd209f7731c81288a63004802f6b141177df154f46e49e898d602a315bd54e13046c92ce34ced5398003546e50e9Q5h49Ebb2KNtmxotuzWfhJZQzx2jhu6FKyGJByrUawM=', '2016-10-01 18:53:19', '2016-10-01 20:32:53', '127.0.0.1', 0);
 
 --
 -- Indexes for dumped tables
@@ -201,6 +253,18 @@ ALTER TABLE `tb_region`
   ADD PRIMARY KEY (`id_region`);
 
 --
+-- Indexes for table `tb_sub_kategori`
+--
+ALTER TABLE `tb_sub_kategori`
+  ADD PRIMARY KEY (`id_sub_kategori`);
+
+--
+-- Indexes for table `tb_super_sub_kategori`
+--
+ALTER TABLE `tb_super_sub_kategori`
+  ADD PRIMARY KEY (`id_super_sub_kategori`);
+
+--
 -- Indexes for table `tb_users`
 --
 ALTER TABLE `tb_users`
@@ -229,7 +293,7 @@ ALTER TABLE `tb_business_profile`
 -- AUTO_INCREMENT for table `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tb_province`
 --
@@ -241,10 +305,20 @@ ALTER TABLE `tb_province`
 ALTER TABLE `tb_region`
   MODIFY `id_region` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
+-- AUTO_INCREMENT for table `tb_sub_kategori`
+--
+ALTER TABLE `tb_sub_kategori`
+  MODIFY `id_sub_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tb_super_sub_kategori`
+--
+ALTER TABLE `tb_super_sub_kategori`
+  MODIFY `id_super_sub_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT for table `tb_users`
 --
 ALTER TABLE `tb_users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
