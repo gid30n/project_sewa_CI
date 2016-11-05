1874b878cbd72fe4ee6ef6f5aeafba504447b861
tukangbatu <dewamah@gmail.com>
2016-11-05 13:41:01 +0800

    database: nambah no_tlp

--- sewania.sql
+++ sewania.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 01, 2016 at 10:34 AM
+-- Generation Time: Nov 05, 2016 at 05:39 AM
 -- Server version: 5.5.50-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.19
 
@@ -81,14 +81,19 @@ CREATE TABLE IF NOT EXISTS `tb_business_profile` (
   `description` text NOT NULL,
   `id_user` int(11) NOT NULL,
   PRIMARY KEY (`id_business_profile`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `tb_business_profile`
 --
 
 INSERT INTO `tb_business_profile` (`id_business_profile`, `brand_name`, `type_service`, `address`, `id_province`, `id_region`, `description`, `id_user`) VALUES
-(1, 'uik dev', 'tes', 'jl anu', 1, 6, 'tes', 16);
+(1, 'uik dev', 'tes', 'jl anu', 1, 6, 'tes', 16),
+(2, 'AAAAAAAA', 'adadad', 'adadnjanjdknjands', 1, 7, 'asdadads', 21),
+(3, 'AAAAAAAAn', 'adadasdasdasdsa', 'asasa', 1, 7, 'saddasdas', 22),
+(4, 'iuuasdhakjhd', 'SSSASDASDASD', 'SADADAD', 1, 1, 'dasdadasd', 23),
+(5, 'Test', 'adsasdasd', 'dasdkadjhajkshd', 1, 1, 'adasbdjabsdbasjhd', 24),
+(6, 'sasa', 'sadasdad', 'lkadhajdhjhadkjas', 1, 1, 'asdasdasdasdasd', 25);
 
 -- --------------------------------------------------------
 
@@ -151,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `tb_konsult` (
   `status` tinyint(1) NOT NULL,
   `id_region` int(11) NOT NULL,
   PRIMARY KEY (`id_konsult`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tb_konsult`
@@ -159,7 +164,8 @@ CREATE TABLE IF NOT EXISTS `tb_konsult` (
 
 INSERT INTO `tb_konsult` (`id_konsult`, `name`, `no_telp`, `acara`, `tamu`, `descript`, `start`, `tgl_konsultasi`, `status`, `id_region`) VALUES
 (3, 'aaaa', 'aaaa', '6', '2', '<p>asdasdasdas asasdasdasd</p>\n<p>sd</p>', '2016-10-20', '2016-10-30', 1, 7),
-(4, 'aaaa', '121223131', '8', '1', '<p>sdadasd</p>', '2016-10-11', '2016-10-31', 1, 1);
+(4, 'aaaa', '121223131', '8', '1', '<p>sdadasd</p>', '2016-10-11', '2016-10-31', 1, 1),
+(5, 'aaaa', 'aaaa', '8', '4', '<p>sasasas</p>', '2016-11-09', '2016-11-01', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -294,6 +300,7 @@ CREATE TABLE IF NOT EXISTS `tb_users` (
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `no_telp` text NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` text NOT NULL,
   `avatar` text NOT NULL,
@@ -302,17 +309,26 @@ CREATE TABLE IF NOT EXISTS `tb_users` (
   `ip_last` varchar(50) NOT NULL,
   `admin` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
 
 --
 -- Dumping data for table `tb_users`
 --
 
-INSERT INTO `tb_users` (`id_user`, `first_name`, `last_name`, `email`, `username`, `password`, `avatar`, `joined`, `last_login`, `ip_last`, `admin`) VALUES
-(2, 'Komang', 'Suryadana', 'suryadana80@gmail.com', '', '8821716db3b4a9e6708a792da936df9357bb9b455e1600354c54d2009e6deb4275fc1224d33989452af7214bfbccde878174fc56d562ecfcc6f68c08a4fbef4e8pzcODt1xyBIbsuUc9phVgFc4C5RaUTMD1SXm+EdLhA=', '', '0000-00-00 00:00:00', '2016-10-25 22:35:52', '127.0.0.1', -9),
-(15, 'Mikel', 'Eric', 'pendekar_langit@protonmail.com', '', 'dd314dadda935008202a1d1028e923bc0d0bbd209f7731c81288a63004802f6b141177df154f46e49e898d602a315bd54e13046c92ce34ced5398003546e50e9Q5h49Ebb2KNtmxotuzWfhJZQzx2jhu6FKyGJByrUawM=', '', '2016-10-01 18:53:19', '2016-10-27 01:39:07', '127.0.0.1', 0),
-(16, 'Pendekar', 'Langit', 'pendekar@protonmail.com', '', 'eb5f53336f92956e74c8c6d2cb5019001cf8dbd06f8f37f0b6f575e1c04c48710ca3ac41962e9598201a931de3d10aa30fad2149e701926a15de5962148ddc7cz/LpgcuVBLMAmJ5i190Lg4J4+iTCvIcj+N4LC8pTHXQ=', 'uploads/avatar/bdi_under_presaur1.jpg', '2016-10-24 02:48:25', '2016-10-25 22:56:03', '127.0.0.1', 1),
-(20, 'Dewa', 'Mahendra', 'dewamah@gmail.com', '_bogeyman_', '105a9eade3fcfcf66a51164fe98908734108d14885d75efb031bcd0f7289f3b56871233050ed2692d343d7ab2ffdf8a6278f16a67c0ef3b04c3804c17a8c2330hu+iQjJyqmW1t5tXHRwF43kU2kpRILANwfjkCftsZD0=', 'assets/img/ava/1.png', '2016-10-30 08:55:17', '2016-10-30 09:30:52', '192.168.12.1', -9);
+INSERT INTO `tb_users` (`id_user`, `first_name`, `last_name`, `email`, `no_telp`, `username`, `password`, `avatar`, `joined`, `last_login`, `ip_last`, `admin`) VALUES
+(2, 'Komang', 'Suryadana', 'suryadana80@gmail.com', '', '', '8821716db3b4a9e6708a792da936df9357bb9b455e1600354c54d2009e6deb4275fc1224d33989452af7214bfbccde878174fc56d562ecfcc6f68c08a4fbef4e8pzcODt1xyBIbsuUc9phVgFc4C5RaUTMD1SXm+EdLhA=', '', '0000-00-00 00:00:00', '2016-10-25 22:35:52', '127.0.0.1', -9),
+(15, 'Mikel', 'Eric', 'pendekar_langit@protonmail.com', '', '', 'dd314dadda935008202a1d1028e923bc0d0bbd209f7731c81288a63004802f6b141177df154f46e49e898d602a315bd54e13046c92ce34ced5398003546e50e9Q5h49Ebb2KNtmxotuzWfhJZQzx2jhu6FKyGJByrUawM=', '', '2016-10-01 18:53:19', '2016-10-27 01:39:07', '127.0.0.1', 0),
+(16, 'Pendekar', 'Langit', 'pendekar@protonmail.com', '', '', 'eb5f53336f92956e74c8c6d2cb5019001cf8dbd06f8f37f0b6f575e1c04c48710ca3ac41962e9598201a931de3d10aa30fad2149e701926a15de5962148ddc7cz/LpgcuVBLMAmJ5i190Lg4J4+iTCvIcj+N4LC8pTHXQ=', 'uploads/avatar/bdi_under_presaur1.jpg', '2016-10-24 02:48:25', '2016-10-25 22:56:03', '127.0.0.1', 1),
+(20, 'Dewa', 'Mahendra', 'dewamah@gmail.com', '', '_bogeyman_', '105a9eade3fcfcf66a51164fe98908734108d14885d75efb031bcd0f7289f3b56871233050ed2692d343d7ab2ffdf8a6278f16a67c0ef3b04c3804c17a8c2330hu+iQjJyqmW1t5tXHRwF43kU2kpRILANwfjkCftsZD0=', 'assets/img/ava/1.png', '2016-10-30 08:55:17', '2016-11-04 23:15:59', '127.0.0.1', -9),
+(21, 'AAA', 'BBB', 'slashrootctf@gmail.com', '081338315351', 'sasa', 'e605cd18e949254ff3f066f337ca6530c693f7ce64fe9f80feec77a47a3a9883faabb08b0a6a8ee624f3f84ff679273233e60800e9c14d7ea9b42a64094693d7zn6BBsGgWmQK4NMKEzZFBI7WDTlNGiCR/DxhhtFNQks=', 'assets/img/ava/1.png', '2016-11-05 10:39:06', '0000-00-00 00:00:00', '', 1),
+(22, 'aadada', '[kasfksfklsjdfkj', 'dadabesar@yahoo.com', '09283908112213', 'dada', '5d4b0e1c28718360070ea75a75cf32cfa60f69f2152933ca7d397f602dc58e7362fa314992ab9a4e9428d41a2c344b04f40caf12c067eb90a16bcb028342f7968wrUfoWqNf/n2cvBqEU50KfKG0Z8TEZdXfH9Y7A2YWM=', 'assets/img/ava/1.png', '2016-11-05 10:44:23', '0000-00-00 00:00:00', '', 1),
+(23, 'aaaaa', 'aaaaaaaaaaa', 'dadu69@gmail.com', '19823918938938', 'dadi', '579f3b4175c7b65b556b88e3c062f66aa0be69c12175193db4a8d466871c173eb1756ef91d7e879a7ff0f00ed7b668f2998a013e7ff1fec349c8baf937a1b9ddQUwVy43H/pO2S2To5c/memaICAlhYkHwA8+vcksrO8Y=', 'assets/img/ava/1.png', '2016-11-05 10:48:52', '0000-00-00 00:00:00', '', 1),
+(24, 'aaaa', 'cccc', 'test123@test.com', '1023891723971', 'test123', '4e907b876f24a0f2adeafa1a9b43a758c405f7e624485c1fa4558bd30d048a728bcedcc54de259eebd124c389e2a9ef6461a40ede13ff193441ded9eeb9bc46251bburZNcTcT5olG+NESTVJgq2cCjr08Hbc+jTnZsZY=', 'assets/img/ava/1.png', '2016-11-05 11:31:01', '0000-00-00 00:00:00', '', 1),
+(25, 'bbbbbb', 'aaaaaa', 'dewadewa@dewa.com', '12123137981738271798', 'dewa', '67192cecc9ac9bf352e6a15d2f2650fe67266d78057a590eaf5cbe951d1c6a2b6102c71a6039df474464c93c2c16d9ccb70214727636e8334d56f56c8e8999abXx+EyxMXFMkJE3FiJOnGq270BkN9rcCU9j2fpPlRrVU=', 'assets/img/ava/1.png', '2016-11-05 11:34:54', '2016-11-05 11:47:46', '127.0.0.1', 1),
+(26, 'aaaaaaaaaaaaa', 'sadasdasdkjasjkdkl', 'aduhhay@audi.com', '081338315351', 'aduh', 'a4442e9769bad5f31c8a6fab4ed511fc93118105abbc0c88dd0792d90c240af83de8fcfb7b34d988007c95e2bc626dd755d9fd8297b44e6cc8fd1bca5c00f86aN2uEj+e2EqoihzeUhIIzh97R8+4gZQVP5ENmAkmM+0c=', 'assets/img/ava/1.png', '2016-11-05 12:05:53', '0000-00-00 00:00:00', '', 0),
+(27, 'KJJJHJAS', 'DSNJNDKJSNJDK', 'ownta@arab.com', '38789798327928', 'ownta', '52afcbac82a67806378ee173edf51ded55652829ac460e52ebaf402c7cab3a5b5f21645af2fc0f68dbe31d1a6c270a7829251365a4aaa2a92bab551d27532cdf2YXqYh9wIIRqdCjN6JT5xx5frwezr9r5+BYOzlvsZT0=', 'assets/img/ava/1.png', '2016-11-05 12:07:48', '2016-11-05 12:11:48', '127.0.0.1', 0),
+(28, 'Erico', 'erix', 'erix@wrong.com', '32138791823798', 'ngerix', '8c336d594d8d1c177c349d74dc3fa8896e87ccd6018ce59e00d1e4a32a8934cd1fa26a1b2eeaa4110e681a580930d70e5ddeb2c6125dc2452d9af9226bb43be8GBkifDYt2iAFQ3SieblEzlSg4GH/6nS+qde2MM+cQJk=', 'assets/img/ava/1.png', '2016-11-05 12:13:01', '2016-11-05 12:13:50', '127.0.0.1', 0),
+(29, 'test', '123', 'pwnta@onta.com', '9384927384729873948', 'pwnta', '64b3e456349bdec66ad0d71fd3c61460bcce9eef2613f30968b31f0273a51866eb80ac98616e48cfc60893e4933f2a2177d1009c65356e50286cbbf8143d82753hh7Xw6V4fNhaS+1sSizQa5aUj/2RksvkO2y/LpCr0Q=', 'assets/img/ava/1.png', '2016-11-05 12:15:48', '0000-00-00 00:00:00', '', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
