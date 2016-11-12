c3051ab9c098d2d3548c91ef6f000284f6b80acd
tukangbatu <dewamah@gmail.com>
2016-11-12 10:23:11 +0800

    order new

---
+++ application/views/backend/order-new.php
@@ -0,0 +1,55 @@
+<div class="col s12 l9 right">
+				<div class="card white">
+					<div class="card-content teal-text">						
+						<div class="row">
+							<div class="col s12">
+								<div class="col s12">
+									<h5 class="left"><i class="material-icons">shopping_cart</i> Pesan</h5>				
+								</div>								
+								<hr class="hide-on-med-and-down">
+								<div class="col m6 s12">
+									<p>
+      									<input type="checkbox" onclick="checkAll(this)" id="checkAll" />
+      									<label for="checkAll">Select: All | </label>
+      									<a class="waves-effect waves-light red btn"><i class="material-icons right">close</i>Delete</a>
+    								</p>
+								</div>
+								<div class="col m6 s12">
+									<div class="row">
+										<div class="input-field">
+											<div class="col m2 s3">
+												<label for="filter">Search <br>
+                      								<a title="clear filter" class="clear-filter" href="#clear">[clear]</a>
+                      							</label>		
+											</div>
+											<div class="col m10 s9">
+												<input type="text" id="search" name="search" placeholder="filter">
+												<br class="hide-on-med-and-up">
+												<br class="hide-on-med-and-up">
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col s12 m4">
+							          <div class="card">
+							            <div class="card-image">
+							              <img src="<?php echo base_url(); ?>assets/img/ava/1.png">
+							              <span class="card-title bening"><b>Nama Pemesan</b></span>
+							            </div>
+							            <div class="card-content black-text">
+							              <a href="#!" class="black-text"><i class="small material-icons left">perm_contact_calendar</i>01/01/1965</a><br>
+							              <a href="#!" class="black-text"><i class="small material-icons left">price</i>Rp.100101010<span class="new badge" data-badge-caption="Total Item"><b>3</b></span></a><br>							              
+							            </div>
+							            <div class="card-action">
+							              <a href="<?php echo base_url(); ?>dashboard-cus/order/detail/1">Detail</a>
+							            </div>
+							          </div>
+							        </div>							
+								</div>
+							</div>
+						</div>
+					</div>					
+				</div>
+			</div>
+		</div>
